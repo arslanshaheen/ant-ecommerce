38ddc1cd87ba6d15eed83c570f38e42f3aa352c8
arslan <arslanshaheen1999@gmail.com>
2023-11-03 15:23:15 -0500

    Your commit message here

---
+++ src/API/index.js
@@ -0,0 +1,91 @@
+// export const getAllProducts = () => {
+import Category from "./../Pages/Category/index";
+//   return fetch("https://dummyjson.com/products")
+//     .then((res) => res.json())
+//     .then(console.log);
+// };
+export const getAllProducts = async () => {
+  try {
+    const response = await fetch("https://dummyjson.com/products");
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+    const data = await response.json();
+    console.log(data);
+    return data;
+  } catch (error) {
+    console.error("An error occurred:", error);
+  }
+};
+
+// export const getProductsByCategory = (category) => {
+//   return fetch(`https://dummyjson.com/products/category/${category}`).then(
+//     (res) => res.json()
+//   );
+// };
+export const getProductsByCategory = async (category) => {
+  try {
+    const response = await fetch(
+      `https://dummyjson.com/products/category/${category}`
+    );
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("An error occurred:", error);
+    throw error; // Re-throw the error for further handling if needed
+  }
+};
+
+// export const addToCart = (id) => {
+//   return fetch("https://dummyjson.com/carts/add", {
+//     method: "POST",
+//     headers: { "Content-Type": "application/json" },
+//     body: JSON.stringify({
+//       userId: 1,
+//       products: [
+//         {
+//           id: id,
+//           quantity: 1,
+//         },
+//       ],
+//     }),
+//   })
+//     .then((res) => res.json())
+//     .then(console.log);
+// };
+///addtocart//
+export const getCart = () => {
+  return fetch("https://dummyjson.com/carts/1")
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data); // Log the data here
+      return data; // Return the data if needed
+    })
+    .catch((error) => {
+      console.error("An error occurred:", error);
+      throw error; // Re-throw the error for further handling if needed
+    });
+};
+//////////////////
+export const addToCart = (id) => {
+  return fetch("https://dummyjson.com/carts/add", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      userId: 1,
+      products: [
+        {
+          id: id,
+          quantity: 1,
+        },
+      ],
+    }),
+  })
+    .then((res) => res.json())
+    .then(console.log);
+};
---
+++ src/Components/Products/index.js
@@ -0,0 +1,134 @@
+import {
+  Button,
+  Image,
+  Card,
+  List,
+  Typography,
+  Badge,
+  Rate,
+  message,
+  Spin,
+} from "antd";
+import { useEffect, useState } from "react";
+import {
+  addToCart,
+  getAllProducts,
+  getProductsByCategory,
+} from "./../../API/index";
+import { useParams } from "react-router-dom";
+
+function Products() {
+  const [loading, setLoading] = useState(false);
+  const param = useParams();
+  const [items, setItems] = useState([]);
+  useEffect(() => {
+    setLoading(true);
+    (param.categoryId
+      ? getProductsByCategory(param.categoryId)
+      : getAllProducts()
+    ).then((res) => {
+      setItems(res.products);
+      setLoading(false);
+    });
+  }, [param]);
+  if (loading) {
+    return <Spin spinning />;
+  }
+  return (
+    <div>
+      <List
+        grid={{ column: 3 }}
+        renderItem={(product, index) => {
+          return (
+            <Badge.Ribbon
+              className="badgeName"
+              text={product.discountPercentage}
+              color="pink"
+            >
+              <Card
+                className="itemCard"
+                title={product.title}
+                key={index}
+                cover={
+                  <Image className="itemCardImage" src={product.thumbnail} />
+                }
+                actions={[
+                  <Rate allowHalf disabled value={product.rating} />,
+                  <AddToCartButton item={product} />,
+                ]}
+              >
+                <Card.Meta
+                  title={
+                    <Typography.Paragraph>
+                      Price:${product.price}{" "}
+                      <Typography.Text delete type="danger">
+                        $
+                        {parseFloat(
+                          product.price +
+                            (product.price * product.discountPercentage) / 100
+                        ).toFixed(2)}
+                      </Typography.Text>
+                    </Typography.Paragraph>
+                  }
+                  description={
+                    <Typography.Paragraph
+                      ellipsis={{ rows: 2, expandable: true, symbol: "more" }}
+                    >
+                      {product.description}
+                    </Typography.Paragraph>
+                  }
+                ></Card.Meta>
+              </Card>
+            </Badge.Ribbon>
+          );
+        }}
+        dataSource={items}
+      ></List>
+    </div>
+  );
+}
+// function AddToCartButton({ item }) {
+//     const  [loading,setLoading]=useState(false)
+//   const addProductToCart = () => {
+//     setLoading(true)
+//     addToCart(item.id).then((res) => {
+//       message.success(`${item.title} has been added to cart!`);
+//       setLoading(false)
+//     });
+//   };
+//   return (
+//     <Button
+//       type="link"
+//       onClick={() => {
+//         addProductToCart;
+//       }}
+//     >
+//       Add to Cart
+//     </Button>
+//   );
+// }
+//new way
+
+function AddToCartButton({ item }) {
+  const [loading, setLoading] = useState(false);
+
+  const addProductToCart = async () => {
+    setLoading(true);
+    try {
+      await addToCart(item.id);
+      message.success(`${item.title} has been added to the cart!`);
+    } catch (error) {
+      console.error("Error adding to cart:", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <Button type="link" onClick={addProductToCart} loading={loading}>
+      Add to Cart
+    </Button>
+  );
+}
+
+export default Products;
